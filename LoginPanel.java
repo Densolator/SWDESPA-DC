77c5faa40ab1faa1caac4759641ab0da3ddb6323
luisthemartinez <48746042+luisthemartinez@users.noreply.github.com>
2019-03-26 17:23:53 +0800

    Add files via upload
    Added login successful and failed

--- LoginPanel.java
+++ LoginPanel.java
@@ -1,11 +1,14 @@
 
 import java.awt.*;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 
 public class LoginPanel extends JPanel{
-	private JTextField username, password;
+	private JTextField username;
+	private JPasswordField password;
 	private JLabel usernameLabel, passwordLabel, signupLabel;
 	private JButton loginButton, signupButton;
 	private Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
@@ -24,8 +27,27 @@ public class LoginPanel extends JPanel{
 		passwordLabel = new JLabel("Password: ");
 		signupLabel = new JLabel("Don't have an account yet?");
 		username = new JTextField();
-		password = new JTextField();
+		password = new JPasswordField();
 		loginButton = new JButton("Log in");
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "");
+					Statement stmt = con.createStatement();
+					String sql = "SELECT * FROM user WHERE username='"+username.getText()+"' and password='"+password.getText().toString()+"'";
+					ResultSet rs = stmt.executeQuery(sql);
+					if(rs.next())
+						JOptionPane.showMessageDialog(null, "Login Successful");
+					else
+						JOptionPane.showMessageDialog(null, "Login Failed");
+					con.close();
+				}
+				catch(Exception e){
+					System.out.print(e);
+				}
+			}
+		});
 		signupButton = new JButton("Sign up");
 		
 		this.setLayout(new GridLayout(7,2));
