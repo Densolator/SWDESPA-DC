8d9343d2cefd267cfdc35c9fcbf8b241a003aab9
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2019-03-29 01:34:34 +0800

    updated gui, added jlist

--- ProfilePanel.java
+++ ProfilePanel.java
@@ -22,6 +22,16 @@ public class ProfilePanel extends JPanel{
 		label1.setBounds(100, 60, 200, 100);
 		add(label1);
 
+		//array of favorite playlists
+		DefaultListModel<String> name = new DefaultListModel<>();  
+		name.addElement("Playlist1");  
+		name.addElement("Playlist2");  
+		name.addElement("Playlist3");   
+				       
+		JList<String> favList = new JList<>(name);  
+		favList.setBounds(50, 125, 200, 500);
+		add(favList);
+		
 		label3 = new JLabel ("Add favorite playlist: ");
 		label3.setBounds(20, 25, 200, 100);
 		add(label3);
@@ -43,6 +53,16 @@ public class ProfilePanel extends JPanel{
 		label4 = new JLabel ("Add favorite songs: ");
 		label4.setBounds(345, 25, 200, 100);
 		add(label4);
+		
+		//array of favorite songs
+		DefaultListModel<String> title = new DefaultListModel<>();  
+		title.addElement("Song1");  
+		title.addElement("Song2");  
+		title.addElement("Song3");   
+		       
+		JList<String> favSongs = new JList<>(title);  
+		favSongs.setBounds(400, 125, 200, 500);
+		add(favSongs);
 		
 		cmbSongs = new JComboBox();
 		cmbSongs.setBounds(460, 65, 100, 25);
