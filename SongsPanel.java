39cc92d2b74b75ccc3a6ec454b33cede9e84004f
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2019-03-29 02:32:58 +0800

    updated gui

--- SongsPanel.java
+++ SongsPanel.java
@@ -3,16 +3,32 @@ import java.awt.*;
 import javax.swing.*;
 public class SongsPanel extends JPanel{
 	private JLabel label;
+	private JTable tblSongs;
+	private JButton btnAdd;
 	
 	public SongsPanel()
 	{
-		setLayout(new FlowLayout());
+		setLayout(new FlowLayout(50, 50, 50));
 		instantiate();
 	}
 	
 	private void instantiate()
 	{
-		label = new JLabel("This panel is for the User's songs");
+		label = new JLabel("My Songs");
+		//label.setBounds(300, 1, 200,100);
 		add(label);
+		
+		//sample output
+		String[] columns = {"Title", "Album", "Genre", "Year"};
+		Object[][] data = {{"Hello", "Adele", "Pop", "2010"}};
+		
+		tblSongs = new JTable (data, columns);
+		tblSongs.setPreferredScrollableViewportSize(new Dimension(500,250));
+		tblSongs.setFillsViewportHeight(true);
+		JScrollPane scrollpane = new JScrollPane(tblSongs);
+		add(scrollpane);
+		
+		btnAdd = new JButton ("Add Songs");
+		add(btnAdd);
 	}
 }
