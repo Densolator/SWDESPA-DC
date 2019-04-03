dce56627cf62abc2462131774030a889a281f9f6
luisthemartinez <48746042+luisthemartinez@users.noreply.github.com>
2019-04-03 10:39:17 +0800

    Update ProfilePanel.java
    Updated

--- ProfilePanel.java
+++ ProfilePanel.java
@@ -1,20 +1,47 @@
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 import javax.swing.*;
 
 import loginModule.LoginController;
+import profileModule.ProfileController;
+
 public class ProfilePanel extends JPanel{
 	private ProfileController controller = new ProfileController();
 	private JLabel label, label1, label2, label3, label4;
 	private JComboBox cmbPlaylist, cmbSongs;
 	private JButton btnAdd1, btnAdd2;
-	
+
+	JList<PlaylistItem> favList = new JList<>();
+	JList<SongItem> favSongs = new JList<>();
 	public ProfilePanel()
 	{
 		setLayout(null);
 		instantiate();
 	}
-	
+
+	public void load(int userid) {
+		DefaultListModel<PlaylistItem> playlists = new DefaultListModel<>();
+		HashMap<Integer, String> val = controller.fave(userid, ProfileController.Type.PLAYLIST);
+
+		for (Map.Entry<Integer, String> entry : val.entrySet()) {
+			playlists.addElement(new PlaylistItem(entry.getKey(), entry.getValue()));
+		}
+
+		favList.setModel(playlists);
+
+		DefaultListModel<SongItem> songs = new DefaultListModel<>();
+		HashMap<Integer, String> vals = controller.fave(userid, ProfileController.Type.SONG);
+
+		for (Map.Entry<Integer, String> entry : vals.entrySet()) {
+			songs.addElement(new SongItem(entry.getKey(), entry.getValue()));
+		}
+
+		favSongs.setModel(songs);
+	}
+
 	private void instantiate()
 	{
 				label = new JLabel("Profile");
@@ -24,14 +51,7 @@ public class ProfilePanel extends JPanel{
 		label1 = new JLabel("My Favorite Playlists");
 		label1.setBounds(100, 60, 200, 100);
 		add(label1);
-		
-		//array of favorite playlists
-		DefaultListModel<String> name = new DefaultListModel<>();  
-		name.addElement("Playlist1");  
-		name.addElement("Playlist2");  
-		name.addElement("Playlist3");   
-				       
-		JList<String> favList = new JList<>(name);  
+
 		favList.setBounds(50, 125, 200, 500);
 		add(favList);
 
@@ -58,12 +78,6 @@ public class ProfilePanel extends JPanel{
 		add(label4);
 		
 		//array of favorite songs
-		DefaultListModel<String> title = new DefaultListModel<>();  
-		title.addElement("Song1");  
-		title.addElement("Song2");  
-		title.addElement("Song3");   
-		       
-		JList<String> favSongs = new JList<>(title);  
 		favSongs.setBounds(400, 125, 200, 500);
 		add(favSongs);
 		
@@ -75,5 +89,35 @@ public class ProfilePanel extends JPanel{
 		btnAdd2.setBounds(565, 65, 70, 25);
 		add(btnAdd2);
 		
+	}
+
+	class PlaylistItem {
+		public int id;
+		public String name;
+
+		public PlaylistItem(int id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return this.name;
+		}
+	}
+
+	class SongItem {
+		public int id;
+		public String name;
+
+		public SongItem(int id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return this.name;
+		}
 	}
 }
