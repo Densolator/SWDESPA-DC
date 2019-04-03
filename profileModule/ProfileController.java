1c7e67d839a5aa36e0a4796f6bca50a1f3f4c6ce
luisthemartinez <48746042+luisthemartinez@users.noreply.github.com>
2019-04-03 10:38:11 +0800

    Update ProfileController.java
    Updated

--- profileModule/ProfileController.java
+++ profileModule/ProfileController.java
@@ -1,34 +1,47 @@
 package profileModule;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JOptionPane;
 
 public class ProfileController {
+	public enum Type{
+		PLAYLIST, SONG
+	}
 	
-	public void profile(String username, String password) {
+	public HashMap<Integer, String> fave(int userid, Type type) {
 		
 		PreparedStatement preparedStatement = null;
 		
-		String sql = "SELECT * FROM playlist WHERE userid = ?";
-		
+		String sql;
+
+		if (type == Type.PLAYLIST){
+			sql = "SELECT * FROM playlist P INNER JOIN favoriteplaylists FP ON P.playlistid = FP.playlistid WHERE FP.userid = ?";
+		} else {
+			sql = "SELECT * FROM song S INNER JOIN favoritesongs FS ON S.songid = FS.songid WHERE FS.userid = ?";
+		}
+
+		HashMap<Integer, String> playlists = new HashMap<>();
 		try {
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
 			preparedStatement = con.prepareStatement(sql);
 			preparedStatement.setInt(1, 1);
 			
 			ResultSet rs = preparedStatement.executeQuery();
-			
 			while (rs.next()) {
-
-				String playlistname = rs.getString("playlistname");
-
-				System.out.println("Playlist Name: " + playlistname);
+				if (type == Type.PLAYLIST) {
+					playlists.put(rs.getInt("playlistid"), rs.getString("playlistname"));
+				} else {
+					playlists.put(rs.getInt("songid"), rs.getString("songname"));
+				}
 			}
 		}
-		
 		catch(Exception e) {
 			e.printStackTrace();
+		} finally {
+			return playlists;
 		}
 	}
 }
